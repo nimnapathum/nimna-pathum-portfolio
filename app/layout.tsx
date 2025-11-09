6d451846a2bb3bce0276c621dab3efe55f3a0764
Nimna Pathum <113501913+nimnapathum@users.noreply.github.com>
2025-11-09 21:38:31 +0530

    update to nextJs

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const SansCode = localFont({
+  src: "../public/fonts/GoogleSansCode.ttf"
+});
+
+export const metadata: Metadata = {
+  title: "Nimna Pathum",
+  description: "Confessions of a Software Engineer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${SansCode.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
